1b85115bb3ddf5bdd35684e559a650f3793186db
Seivan Heidari <seivan@kth.se>
2014-01-09 13:52:59 +0100

    Clean things up.

--- RUBSPasswordStrength/RUBSPasswordStrength.m
+++ RUBSPasswordStrength/RUBSPasswordStrength.m
@@ -3,68 +3,51 @@
 #define REGEX_PASSWORD_ONE_NUMBER @"^(?=.*[0-9]).*$"  //Should contains one or more number
 #define REGEX_PASSWORD_ONE_SYMBOL @"^(?=.*[!@#$%&_]).*$"  //Should contains one or more symbol
 
-@interface RegExUtils : NSObject
 
-typedef enum {
-    PasswordStrengthTypeWeak,
-    PasswordStrengthTypeModerate,
-    PasswordStrengthTypeStrong
-}PasswordStrengthType;
+#import "RUBSPasswordStrength.h"
 
-+ (PasswordStrengthType)checkPasswordStrength:(NSString *)password;
+@implementation RUBSPasswordStrength
 
-@end
++(RUBSPasswordStrengthType)checkPasswordStrength:(NSString *)password {
+  NSInteger len = password.length;
+  //will contains password strength
+  NSInteger strength = 0;
+  
+  if (len == 0) return RUBSPasswordStrengthTypeWeak;
+  else if (len <= 5) strength++;
+  else if (len <= 10)  strength += 2;
+  else strength += 3;
 
-@implementation RegExUtils
-
-+(PasswordStrengthType)checkPasswordStrength:(NSString *)password {
-    int len = password.length;
-    //will contains password strength
-    int strength = 0;
-
-    if (len == 0) {
-        return PasswordStrengthTypeWeak;
-    } else if (len <= 5) {
-        strength++;
-    } else if (len <= 10) {
-        strength += 2;
-    } else{
-        strength += 3;
-    }
-
-    strength += [self validateString:password withPattern:REGEX_PASSWORD_ONE_UPPERCASE caseSensitive:YES];
-    strength += [self validateString:password withPattern:REGEX_PASSWORD_ONE_LOWERCASE caseSensitive:YES];
-    strength += [self validateString:password withPattern:REGEX_PASSWORD_ONE_NUMBER caseSensitive:YES];
-    strength += [self validateString:password withPattern:REGEX_PASSWORD_ONE_SYMBOL caseSensitive:YES];
-
-    if(strength <= 3){
-        return PasswordStrengthTypeWeak;
-    }else if(3 < strength && strength < 6){
-        return PasswordStrengthTypeModerate;
-    }else{
-        return PasswordStrengthTypeStrong;
-    }
+  
+  strength += [self validateString:password withPattern:REGEX_PASSWORD_ONE_UPPERCASE caseSensitive:YES];
+  strength += [self validateString:password withPattern:REGEX_PASSWORD_ONE_LOWERCASE caseSensitive:YES];
+  strength += [self validateString:password withPattern:REGEX_PASSWORD_ONE_NUMBER caseSensitive:YES];
+  strength += [self validateString:password withPattern:REGEX_PASSWORD_ONE_SYMBOL caseSensitive:YES];
+  
+  if(strength <= 3) return RUBSPasswordStrengthTypeWeak;
+  else if(3 < strength && strength < 6) return RUBSPasswordStrengthTypeModerate;
+  else return RUBSPasswordStrengthTypeStrong;
 }
 
 // Validate the input string with the given pattern and
 // return the result as a boolean
-+ (int)validateString:(NSString *)string withPattern:(NSString *)pattern caseSensitive:(BOOL)caseSensitive
-{
-    NSError *error = nil;
-    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern options:((caseSensitive) ? 0 : NSRegularExpressionCaseInsensitive) error:&error];
 
-    NSAssert(regex, @"Unable to create regular expression");
-
-    NSRange textRange = NSMakeRange(0, string.length);
-    NSRange matchRange = [regex rangeOfFirstMatchInString:string options:NSMatchingReportProgress range:textRange];
-
-    BOOL didValidate = 0;
-
-    // Did we find a matching range
-    if (matchRange.location != NSNotFound)
-        didValidate = 1;
-
-    return didValidate;
++(BOOL)validateString:(NSString *)string withPattern:(NSString *)pattern caseSensitive:(BOOL)caseSensitive; {
+  NSError *error = nil;
+  NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern
+                                                                         options:((caseSensitive) ? 0 : NSRegularExpressionCaseInsensitive) error:&error];
+  
+  NSAssert(regex, @"Unable to create regular expression");
+  
+  NSRange textRange = NSMakeRange(0, string.length);
+  NSRange matchRange = [regex rangeOfFirstMatchInString:string options:NSMatchingReportProgress range:textRange];
+  
+  BOOL didValidate = 0;
+  
+  // Did we find a matching range
+  if (matchRange.location != NSNotFound) didValidate = 1;
+  
+  return didValidate;
 }
 
 @end
